8bda94ef910418bd35f7c0174da354111b357722
BhavnishYadav <bhavnish15yadav@gmail.com>
2025-01-29 10:09:53 +0000

    com

---
+++ 01basic/04_comparison.js
@@ -0,0 +1,8 @@
+console.log("2" == 2)//its automalttically convert "2" to number because not checking its type
+console.log("2" === 2)//strictlly check type then compare values
+console.log(null == 0)
+console.log(null > 0)
+console.log(null >= 0)//because of js lagging
+console.log(undefined == 0)//because of js lagging
+console.log(undefined > 0)//because of js lagging
+console.log(undefined >= 0)//because of js lagging
